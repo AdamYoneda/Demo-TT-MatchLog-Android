1582b46b9306d6cae05f8cf61a5afd0cc0ff45f5
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-27 05:37:06 +0900

    update: 既存ユーザーログインのコードをドキュメント通りに追加

--- app/src/main/java/com/example/myapplication_2/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication_2/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication_2
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -30,6 +33,28 @@ class LoginActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        // ログインさせる
+        binding.continueBtn.setOnClickListener {
+            auth.signInWithEmailAndPassword(binding.email.getText().toString().trim(), binding.password.getText().toString().trim())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
     }
 
     public override fun onStart() {
