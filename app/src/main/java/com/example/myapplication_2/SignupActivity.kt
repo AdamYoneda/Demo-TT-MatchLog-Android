b4eee81e51d2e5ef3a25e76f7bfdd0478d8d1cd3
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-27 06:20:00 +0900

    fix: onCreate内にあったデフォルトのコードを削除するとエミュレータが落ちる現象がなくなった。

--- app/src/main/java/com/example/myapplication_2/SignupActivity.kt
+++ app/src/main/java/com/example/myapplication_2/SignupActivity.kt
@@ -23,13 +23,7 @@ class SignupActivity : AppCompatActivity() {
 
         binding = ActivitySignupBinding.inflate(layoutInflater)
 
-        enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         // Initialize Firebase Auth
         auth = Firebase.auth
