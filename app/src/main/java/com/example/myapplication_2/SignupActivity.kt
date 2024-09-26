79670fd0612f83254c1bfe34b8c726c2f2ae9ad8
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-27 04:35:58 +0900

    update: SignupActivity内でbinding関連のコードを追加

--- app/src/main/java/com/example/myapplication_2/SignupActivity.kt
+++ app/src/main/java/com/example/myapplication_2/SignupActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication_2.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
