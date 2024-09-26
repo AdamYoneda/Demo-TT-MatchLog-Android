a5c4618657d929b3c4591b86372b057285993f02
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-09-27 05:22:35 +0900

    update: 新規ユーザー登録のコードをドキュメント通りに追加

--- app/src/main/java/com/example/myapplication_2/SignupActivity.kt
+++ app/src/main/java/com/example/myapplication_2/SignupActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication_2
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -30,6 +33,30 @@ class SignupActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth
         auth = Firebase.auth
+
+        // 新規ユーザーを登録
+        binding.continueBtn.setOnClickListener {
+            // trimはホワイトスペースを削除
+            auth.createUserWithEmailAndPassword(binding.email.getText().toString().trim(), binding.password.getText().toString().trim())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        // 画面遷移
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
     }
 
     public override fun onStart() {
