3edd2b01e3cd5b7cb66e2ad4b51b65e3dec1d6ab
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-02 22:49:53 +0900

    remove: 不要なコードの削除

--- app/src/main/java/com/example/myapplication_2/view/MatchRecordActivity.kt
+++ app/src/main/java/com/example/myapplication_2/view/MatchRecordActivity.kt
@@ -10,12 +10,7 @@ import com.example.myapplication_2.R
 class MatchRecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_match_record)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
