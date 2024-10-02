967eee833e0e8974df0ab1f5ee3a790cc34c289a
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-03 03:25:46 +0900

    update: モデルTournamentにカラムを追加。

--- app/src/main/java/com/example/myapplication_2/model/Tournament.kt
+++ app/src/main/java/com/example/myapplication_2/model/Tournament.kt
@@ -1,8 +1,14 @@
 package com.example.myapplication_2.model
 
-// 大会セクション (日付と大会名)
-data class Tournament (
-    val date: String,
-    val tournamentName: String,
-    val matches: List<Match>
+// 大会セクション (日付、試合形式、大会名、試合リスト)
+data class Tournament(
+    val date: String,                     // 大会日付
+    val tournamentName: String,           // 大会名
+    val matchType: MatchType,             // 試合形式（シングルス、ダブルス、団体戦）
+    val matches: List<Match>              // 試合リスト
 )
+
+// 試合形式の列挙型
+enum class MatchType {
+    SINGLES, DOUBLES, TEAM
+}
