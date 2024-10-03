caaf0969a90e38a50d4d172c8a72f39f274a6e0b
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-04 02:38:30 +0900

    update: RecyclerAdapterにRecyclerView.Adapterを継承させ、ViewHolderを指定する。また、Implementする。

--- app/src/main/java/com/example/myapplication_2/view/RecyclerAdapter.kt
+++ app/src/main/java/com/example/myapplication_2/view/RecyclerAdapter.kt
@@ -1,4 +1,18 @@
 package com.example.myapplication_2.view
 
-class RecyclerAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter : RecyclerView.Adapter<MatchViewHolderItem>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MatchViewHolderItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MatchViewHolderItem, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
