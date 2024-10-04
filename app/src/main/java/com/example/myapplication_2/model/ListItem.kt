5663b7aa07c82d9c226bd7bc6b04f5a48cadd7df
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-05 08:55:51 +0900

    remove: ListItem

--- app/src/main/java/com/example/myapplication_2/model/ListItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication_2.model
-
-sealed class ListItem {
-    data class TournamentItem(val tournament: Tournament) : ListItem()
-    data class MatchItem(val match: Match) : ListItem()
-}
