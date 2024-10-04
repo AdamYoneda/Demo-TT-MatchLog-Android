8d3648cdb93497011b8480f8b2e5e1af3c270569
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-04 18:52:12 +0900

    add: TournamentViewHolderをxmlと接続。

--- app/src/main/java/com/example/myapplication_2/view/TournamentViewHolder.kt
+++ app/src/main/java/com/example/myapplication_2/view/TournamentViewHolder.kt
@@ -1,4 +1,15 @@
 package com.example.myapplication_2.view
 
-class TournamentViewHolder {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication_2.R
+import com.example.myapplication_2.model.Tournament
+
+class TournamentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(tournament: Tournament) {
+        // 各大会のデータをバインド
+        itemView.findViewById<TextView>(R.id.tournament_name).text = tournament.tournamentName
+        itemView.findViewById<TextView>(R.id.tournament_date).text = tournament.date
+    }
 }
