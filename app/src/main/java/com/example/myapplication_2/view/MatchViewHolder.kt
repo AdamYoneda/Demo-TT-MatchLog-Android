31c944f2668d87f6609325d2de8cbda5a9d9c357
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-04 18:53:31 +0900

    add: MatchViewHolderをxmlと接続。

--- app/src/main/java/com/example/myapplication_2/view/MatchViewHolder.kt
+++ app/src/main/java/com/example/myapplication_2/view/MatchViewHolder.kt
@@ -1,4 +1,16 @@
 package com.example.myapplication_2.view
 
-class MatchViewHolder {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication_2.R
+import com.example.myapplication_2.model.Match
+
+class MatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(match: Match) {
+        // 試合のデータをバインド
+        itemView.findViewById<TextView>(R.id.opponent_name).text = match.opponentName
+        itemView.findViewById<TextView>(R.id.score).text =
+            "${match.playerScore} - ${match.opponentScore}"
+    }
 }
