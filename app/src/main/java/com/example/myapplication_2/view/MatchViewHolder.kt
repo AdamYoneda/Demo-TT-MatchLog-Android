55a316010127927167bd2ae55c473372c095bf3b
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-05 13:50:48 +0900

    update: 「n回戦」を表示。

--- app/src/main/java/com/example/myapplication_2/view/MatchViewHolder.kt
+++ app/src/main/java/com/example/myapplication_2/view/MatchViewHolder.kt
@@ -9,7 +9,7 @@ import com.example.myapplication_2.model.Match
 class MatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(match: Match) {
         // 試合のデータをバインド
-        itemView.findViewById<TextView>(R.id.opponent_name).text = match.opponentName
+        itemView.findViewById<TextView>(R.id.opponent_name).text = "${match.roundNumber}回戦 ${match.opponentName}"
         itemView.findViewById<TextView>(R.id.score).text =
             "${match.playerScore} - ${match.opponentScore}"
     }
