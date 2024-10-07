c3a2ce4aef43d566accd76950a6bd4f4faee26b0
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-07 14:53:25 +0900

    add: TournamentRepository

---
+++ app/src/main/java/com/example/myapplication_2/data/repository/TounamentRepository.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication_2.data
+
+import com.example.myapplication_2.data.model.Tournament
+
+// TournamentRepository.kt
+class TournamentRepository {
+
+    fun getTournaments(): List<Tournament> {
+        // tournamentsデータをここに移動する
+        return listOf(
+            // 同じtournamentデータ
+        )
+    }
+}
