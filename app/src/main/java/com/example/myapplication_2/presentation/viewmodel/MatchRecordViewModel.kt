399d0ef717d9aeb1c843c1f576856ce26e775cf7
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-07 14:53:13 +0900

    add: MatchRecordViewModel.kt

---
+++ app/src/main/java/com/example/myapplication_2/presentation/viewmodel/MatchRecordViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication_2.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.myapplication_2.data.TournamentRepository
+import com.example.myapplication_2.data.model.MatchType
+import com.example.myapplication_2.data.model.Tournament
+
+// MatchRecordViewModel.kt
+class MatchRecordViewModel(private val repository: TournamentRepository) : ViewModel() {
+
+    private val _tournaments = MutableLiveData<List<Tournament>>()
+    val tournaments: LiveData<List<Tournament>> get() = _tournaments
+
+    // データの取得とソートを行う
+    fun loadTournaments() {
+        val tournamentsList = repository.getTournaments()
+        val matchTypeComparator = compareBy<Tournament> { tournament ->
+            when (tournament.matchType) {
+                MatchType.SINGLES -> 1
+                MatchType.DOUBLES -> 2
+                MatchType.TEAM -> 3
+            }
+        }
+
+        // 日付で降順ソートし、さらにmatchTypeでソート
+        val sortedTournaments = tournamentsList
+            .sortedWith(compareByDescending<Tournament> { it.date }.then(matchTypeComparator))
+            .map { tournament ->
+                tournament.copy(matches = tournament.matches.sortedBy { it.roundNumber })
+            }
+
+        _tournaments.value = sortedTournaments
+    }
+}
