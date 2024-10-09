93af7b2841a7c630cf61f2d59afd3c79f38c529d
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-09 11:47:57 +0900

    add: MatchInputActivity

---
+++ app/src/main/java/com/example/myapplication_2/presentation/view/activity/MatchInputActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication_2.presentation.view.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication_2.R
+
+class MatchInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_match_input)
+
+    }
+}
