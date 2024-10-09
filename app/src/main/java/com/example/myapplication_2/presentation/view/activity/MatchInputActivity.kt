bd1aa96927e2d0e6b91ba3305550fdfa9e0de40e
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-10-09 20:55:10 +0900

    update: 各Viewの部品の取得。

--- app/src/main/java/com/example/myapplication_2/presentation/view/activity/MatchInputActivity.kt
+++ app/src/main/java/com/example/myapplication_2/presentation/view/activity/MatchInputActivity.kt
@@ -1,17 +1,19 @@
 package com.example.myapplication_2.presentation.view.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication_2.R
 
 class MatchInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_match_input)
 
+        // Viewの取得
+        val matchListView: ListView = findViewById(R.id.matchListView)
+        val addItemButton: Button = findViewById(R.id.addItemButton)
+        val confirmButton: Button = findViewById(R.id.confirmButton)
     }
 }
